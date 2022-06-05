ce58b04e943fed8168ddcda30c35f57f4f727090
mgroth0 <mgroth49@gmail.com>
2022-06-05 13:06:12 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 modtype = LIB
 
 dependencies {
-//  api(projects.kj.async)
   api(projects.kj.hurricanefx.eye)
-  api(libs.fx.base)
-  api(libs.fx.graphics)
+//  api(libs.fx.base)
+//  api(libs.fx.graphics)
   api(libs.fx.controls)
   api(projects.kj.fx.image)
 }
