a764ada62553de61b9f4ab22c8da62bd5690e115
mgroth0 <mgroth49@gmail.com>
2022-06-05 01:45:44 -0400

    autocommit

--- src/main/kotlin/matt/hurricanefx/tornadofx/media/Media.kt
+++
@@ -1,8 +0,0 @@
-package matt.hurricanefx.tornadofx.media
-
-/*slightly modified code I stole from tornadofx*/
-
-import javafx.scene.media.Media
-import javafx.scene.media.MediaPlayer
-
-fun Media.play() = MediaPlayer(this).play()
