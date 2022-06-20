e53995bc05b7f5505f000394b61acd15da9bbf6c
mgroth0 <mgroth49@gmail.com>
2022-06-20 18:38:28 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
   implementation(kotlin("reflect"))
   api(projects.kj.hurricanefx.hurricanefxEye)
   api(libs.fx.controls)
-  api(projects.kj.fx.fxImage)
+  api(projects.k.fx.fxImage)
   implementation(projects.k.caching)
   implementation(projects.k.stream)
   implementation(projects.k.async)
