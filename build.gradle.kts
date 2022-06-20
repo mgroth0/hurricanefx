b95b2603d92ad32498f8f766fb5b4c8908fc975b
mgroth0 <mgroth49@gmail.com>
2022-06-20 18:19:22 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ dependencies {
   api(projects.kj.hurricanefx.hurricanefxEye)
   api(libs.fx.controls)
   api(projects.kj.fx.fxImage)
-  implementation(projects.kj.caching)
+  implementation(projects.k.caching)
   implementation(projects.k.stream)
   implementation(projects.k.async)
 }
