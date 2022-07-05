ecc6afd0b76a5771dfc5008d94354318a85f865f
mgroth0 <mgroth49@gmail.com>
2022-07-05 15:32:36 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ modtype = LIB
 
 dependencies {
   implementation(kotlin("reflect"))
-  api(projects.k.hurricanefx.hurricanefxEye)
+  api(projects.k.hurricanefx.eye)
   api(libs.fx.controls)
-  api(projects.k.fx.fxImage)
+  api(projects.k.fx.image)
   implementation(projects.k.caching)
   implementation(projects.k.stream)
   implementation(projects.k.async)
