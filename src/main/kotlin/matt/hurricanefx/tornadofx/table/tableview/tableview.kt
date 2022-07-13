4523dd9bd765649bdfd5cda507c45d6e9c73899e
mgroth0 <mgroth49@gmail.com>
2022-07-13 14:56:33 -0400

    autocommit

--- src/main/kotlin/matt/hurricanefx/tornadofx/table/tableview/tableview.kt
+++ src/main/kotlin/matt/hurricanefx/tornadofx/table/tableview/tableview.kt
@@ -3,6 +3,8 @@ package matt.hurricanefx.tornadofx.table.tableview
 import javafx.beans.property.SimpleObjectProperty
 import javafx.scene.control.TableColumn
 import javafx.scene.control.TableView
+import javafx.scene.control.TreeTableColumn
+import javafx.scene.control.TreeTableView
 import matt.hurricanefx.tornadofx.item.column
 import kotlin.reflect.KFunction
 import kotlin.reflect.KProperty1
@@ -24,5 +26,25 @@ fun <S, T> TableView<S>.column(getter: KFunction<T>): TableColumn<S, T> {
 fun <S, T> TableView<S>.column(getter: KProperty1<S, T>): TableColumn<S, T> {
   return column(getter.name) {
 	SimpleObjectProperty(getter.call(it.value))
+  }
+}
+
+/**
+ * Matt was here!
+ */
+@JvmName("coolColumn")
+inline fun <reified S, T> TreeTableView<S>.column(getter: KFunction<T>): TreeTableColumn<S, T> {
+  return column(getter.name) {
+	SimpleObjectProperty(getter.call(it.value.value))
+  }
+}
+
+/**
+ * Matt was here!
+ */
+@JvmName("coolColumn2")
+inline fun <reified S, T> TreeTableView<S>.column(getter: KProperty1<S, T>): TreeTableColumn<S, T> {
+  return column(getter.name) {
+	SimpleObjectProperty(getter.call(it.value.value))
   }
 }
